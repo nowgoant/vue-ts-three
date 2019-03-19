893a7db10334104e45fc991d5274bcb5efa1022e
majun36 <majun1@jd.com>
2019-03-19 12:18:23 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    {{computedMsg}}
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
@@ -25,19 +24,34 @@ export default class Home extends Vue {
   }
 
   mounted() {
+    // this.runWait();
+
+    // Initiate function or other initializations here
+    this.init();
+  }
+
+  async init() {
+    const width = window.innerWidth * 1;
+    const height = window.innerHeight * 1;
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width, height);
+    renderer.domElement.style.width = `${window.innerWidth}px`;
+    renderer.domElement.style.height = `${window.innerHeight}px`;
     document.body.appendChild(renderer.domElement);
 
+    // 创建模型
     var geometry = new THREE.BoxGeometry(1, 1, 1);
+    // 创建材质
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // 生产着色器
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
     camera.position.z = 5;
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     var animate = function() {
       requestAnimationFrame(animate);
@@ -49,6 +63,41 @@ export default class Home extends Vue {
     };
 
     animate();
+  }
+
+  async initLine() {
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 20);
+    camera.lookAt(0, 0, 0);
+
+    var scene = new THREE.Scene();
+
+    var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+    geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+    geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+
+    var line = new THREE.Line(geometry, material);
+
+    scene.add(line);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.render(scene, camera);
+  }
+
+  async runWait() {
+    await this.delay(2000);
+
+    window.console.log('2');
+  }
+
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   @Watch('child')
